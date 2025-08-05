4a7addad5b880bb755a05ade3e96dcd2d435cf7e
SAM <s.artur.m87@gmail.com>
2025-08-05 14:42:54 -0700

    Вторая часть:
    Создание короткой ссылки
    Создание оригинальной ссылки

---
+++ src/main/java/faang/school/urlshortenerservice/dto/UrlRequestDto.java
@@ -0,0 +1,20 @@
+package faang.school.urlshortenerservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlRequestDto {
+
+    @NotBlank(message = "URL cannot be empty")
+    @Pattern(
+        regexp = "^(https?://)[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$",
+        message = "Invalid URL format"
+    )
+    private String url;
+}
