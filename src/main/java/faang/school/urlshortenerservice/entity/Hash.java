ffd7a2f747c75b40f78a95073eeda80db0d4fb1d
Linempy <mychinnik@yandex.ru>
2025-09-10 21:54:41 +0300

    BJS2-84388-hash_repository - написал репозиторий - создал сущность Hash

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Hash.java
@@ -0,0 +1,27 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Класс-сущность представляющая хэш
+ *
+ * @author Linempy
+ * @since 10.09.2025
+ */
+@Entity
+@Table(name = "hash")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Hash {
+
+    @Id
+    @Column(name = "hash", length = 6)
+    private final String hash;
+}
---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,37 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Интерфейс для сохранения, получения хэшей и получения уникальных чисел для хэширования
+ *
+ * @author Linempy
+ * @since 10.09.2025
+ */
+public interface HashRepository extends JpaRepository<Hash, String> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT nextval('unique_number_seq') FROM generate_series(1, :range)
+            """)
+    Set<Long> getUniqueNumbers(@Param("range") int range);
+
+    @Modifying
+    @Query(nativeQuery = true, value = """
+            DELETE FROM hash
+            WHERE hash IN (
+                SELECT hash FROM hash
+                LIMIT :count
+                FOR UPDATE SKIP LOCKED
+            )
+            RETURNING hash
+            """)
+    List<String> getHashBatch(@Param("count") int count);
+
+}
