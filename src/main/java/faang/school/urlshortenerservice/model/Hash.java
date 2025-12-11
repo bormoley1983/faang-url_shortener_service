c6bfeb86ceee770391ad9fcd0aacd91f0c84dfb3
ayazs <ayaz-22@yandex.ru>
2025-12-11 15:28:28 +0500

    Controller,Redis

--- src/main/java/faang/school/urlshortenerservice/model/Hash.java
+++ src/main/java/faang/school/urlshortenerservice/model/Hash.java
@@ -1,8 +1,6 @@
 package faang.school.urlshortenerservice.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
