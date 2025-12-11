e5f2abd3ecd1e2cd48102cc45441e857ad674946
Mikhail Easy <mghievrie@bk.ru>
2025-12-11 21:27:26 +0500

    import

--- src/test/java/faang/school/urlshortenerservice/service/UrlServiceImplTest.java
+++ src/test/java/faang/school/urlshortenerservice/service/UrlServiceImplTest.java
@@ -6,7 +6,6 @@ import faang.school.urlshortenerservice.repository.HashRepository;
 import faang.school.urlshortenerservice.repository.LocalCache;
 import faang.school.urlshortenerservice.repository.UrlCacheRepository;
 import faang.school.urlshortenerservice.repository.UrlRepository;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
