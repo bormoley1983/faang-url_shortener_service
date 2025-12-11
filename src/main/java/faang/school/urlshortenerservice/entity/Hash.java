7a187da2c6ee7ac2f7c8f0299ede6feef8fb4b06
Deathroels <loocyep@gmail.com>
2025-12-11 16:45:33 +0300

    start repository and entity creating

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Hash.java
@@ -0,0 +1,22 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "hash")
+@Data
+public class Hash {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "hash")
+    //TODO доделать сущность
+}
