2600e61d3028d82168008f6b41f9e95d809e9100
ayazs <ayaz-22@yandex.ru>
2025-12-15 00:20:33 +0500

    fixed

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface HashRepository extends JpaRepository<Hash, Long> {
+public interface HashRepository extends JpaRepository<Hash, Hash> {
 
     @Query(nativeQuery = true, value = """ 
             SELECT nextval('unique_number_seq')
--- src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
@@ -3,6 +3,7 @@ package faang.school.urlshortenerservice.service;
 import faang.school.urlshortenerservice.model.Hash;
 import faang.school.urlshortenerservice.repository.HashRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,6 +15,7 @@ import java.util.concurrent.CompletableFuture;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class HashGenerator {
     @Value("${batch.size}")
     private int butchSize;
@@ -24,12 +26,13 @@ public class HashGenerator {
     @Transactional
     @Scheduled(cron = "${scheduler.tasks.generate-batch.cron:0 0 2 * * *}")
     public void generateBatch() {
+        log.info("Generating new batch of hashes");
         List<Long> uniqueNumbers = hashRepository.getUniqueNumbers(butchSize);
-        List<Hash> hashes = base62Encoder.encode(uniqueNumbers);
+        List<Hash> hashes = base62Encoder.encode(uniqueNumbers).stream().map(Hash::new).toList();
         hashRepository.saveAll(hashes);
+        log.info("Generated {} hashes", hashes.size());
     }
 
-    @Transactional
     public List<String> getHashes(int amount) {
         List<Hash> hashes = hashRepository.getHashBatch(amount);
         if (hashes.size() < amount) {
