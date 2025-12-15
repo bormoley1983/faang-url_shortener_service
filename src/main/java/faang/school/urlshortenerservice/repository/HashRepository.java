e713ca4c0c88cab03824141f4f96be52e8963361
Bimcore <vzeten@gmail.com>
2025-12-15 21:18:33 +0100

    Сделал задачи - [x] Создать структуру БД - BJS2-97370 - [x] Генерация списка хэшей - BJS2-97367 - [x] Работа с базой(HashRepository) - BJS2-97372

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,40 @@
+package faang.school.urlshortenerservice.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class HashRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Value("${hash.batch.size:1000}")
+    private int batchSize;
+
+    public List<Long> getUniqueNumbers(int n) {
+        String sql ="SELECT nextval('unique_number_seq') " +
+                "FROM generate_series(1, ?)";
+        return jdbcTemplate.queryForList(sql, Long.class, n);
+    }
+
+    public void save(List<String> hashes) {
+        String sql = "INSERT INTO hash (hash) VALUES (?)";
+
+        jdbcTemplate.batchUpdate(sql, hashes, hashes.size(),
+                (ps, hash) -> ps.setString(1, hash));
+    }
+
+    public List<String> getHashBatch(int n) {
+        String sql = "DELETE FROM hash " +
+                "WHERE hash IN (SELECT hash FROM hash LIMIT ?) " +
+                "RETURNING hash";
+
+        return jdbcTemplate.queryForList(sql, String.class, n);
+    }
+}
