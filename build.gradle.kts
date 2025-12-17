4b69b23ea1e3468a422074a8ce424434d1a1e0b5
Sofiya <sofiia.chebotareva@gmail.com>
2025-12-17 11:01:05 +0300

    shortener - added test

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     /**
      * Test containers
      */
-    implementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
+    implementation(platform("org.testcontainers:testcontainers-bom:1.19.0"))
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
