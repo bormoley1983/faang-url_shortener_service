57fb76903e25be21387141c33141401613b7916e
Oleg_Korovin <olegk.99@mail.ru>
2025-12-19 20:01:20 +0300

    refactor: добавить логи в HashGenerator

--- src/main/java/faang/school/urlshortenerservice/util/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/util/HashGenerator.java
@@ -2,6 +2,7 @@ package faang.school.urlshortenerservice.util;
 
 import faang.school.urlshortenerservice.repository.HashRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class HashGenerator {
 
     @Value("${hash.hash-generator.generate-batch}")
@@ -19,8 +21,10 @@ public class HashGenerator {
 
     @Async("hashGeneratorExecutor")
     public void generateBatch() {
+        log.info("Generating the batch of {} hashes", batch);
         List<Long> numbers = hashRepository.getUniqueNumbers(batch);
         List<String> hashes = base62Encoder.encode(numbers);
         hashRepository.save(hashes);
+        log.info("The batch of {} hashes is generated and saved", batch);
     }
 }
