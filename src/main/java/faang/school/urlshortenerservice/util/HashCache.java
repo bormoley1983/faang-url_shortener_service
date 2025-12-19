8194a915cd2e3be1a8ff8ba0c5538962b8319ad9
Oleg_Korovin <olegk.99@mail.ru>
2025-12-19 21:45:25 +0300

    refactor: добавить логи в HashCache, добавить логику обработки асинхронных запросов

--- src/main/java/faang/school/urlshortenerservice/util/HashCache.java
+++ src/main/java/faang/school/urlshortenerservice/util/HashCache.java
@@ -1,14 +1,18 @@
 package faang.school.urlshortenerservice.util;
 
 import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
+@Slf4j
 public class HashCache {
 
     @Value("${hash.hash-cache.get-batch}")
@@ -21,6 +25,7 @@ public class HashCache {
     private final HashRepository hashRepository;
     private final ExecutorService hashCacheExecutor;
     private final LinkedBlockingQueue<String> cache = new LinkedBlockingQueue<>(limit);
+    private final AtomicBoolean isGenerating = new AtomicBoolean(false);
 
     public HashCache(
             HashGenerator hashGenerator,
@@ -33,12 +38,20 @@ public class HashCache {
 
     public String getHash() {
         try {
-            if (cache.size() < bound) {
-                hashCacheExecutor.execute(() -> cache.addAll(hashRepository.getHashBatch(batch)));
-                hashGenerator.generateBatch();
+            if (cache.size() < bound && isGenerating.compareAndSet(false, true)) {
+                log.info("The size of the cache is less than 20%");
+                CompletableFuture.runAsync(() -> {
+                            cache.addAll(hashRepository.getHashBatch(batch));
+                            hashGenerator.generateBatch();
+                        }, hashCacheExecutor)
+                        .whenComplete((result, ex) -> isGenerating.set(false))
+                        .exceptionally(ex -> {
+                            throw new RuntimeException(ex);
+                        });
             }
             return cache.take();
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
     }
