0e4f87e7280e5db0b0df7c5b6e6d6124dcb2cf9c
Asta <charlyfazbear@gmail.com>
2025-12-20 22:54:08 +0300

    -corrected url-shortener

--- src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
+++ src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
@@ -3,9 +3,11 @@ package faang.school.urlshortenerservice;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients("faang.school.urlshortenerservice.client")
---
+++ src/main/java/faang/school/urlshortenerservice/config/redis/RedisConfig.java
@@ -0,0 +1,83 @@
+package faang.school.urlshortenerservice.config.redis;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import io.lettuce.core.TimeoutOptions;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Configuration
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
+
+        SocketOptions socketOptions = SocketOptions.builder()
+                .connectTimeout(Duration.ofMillis(redisProperties.getConnectTimeout()))
+                .keepAlive(true)
+                .tcpNoDelay(true)
+                .build();
+
+        TimeoutOptions timeoutOptions = TimeoutOptions.builder()
+                .fixedTimeout(Duration.ofMillis(redisProperties.getTimeout()))
+                .build();
+
+        ClientOptions clientOptions = ClientOptions.builder()
+                .socketOptions(socketOptions)
+                .timeoutOptions(timeoutOptions)
+                .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                .autoReconnect(true)
+                .cancelCommandsOnReconnectFailure(false)
+                .suspendReconnectOnProtocolFailure(false)
+                .requestQueueSize(Integer.MAX_VALUE)
+                .build();
+
+        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+        genericObjectPoolConfig.setMaxIdle(5000);
+        genericObjectPoolConfig.setMinIdle(1000);
+        genericObjectPoolConfig.setMaxTotal(5000);
+        genericObjectPoolConfig.setMaxWait(Duration.ofMillis(2000));
+
+        LettucePoolingClientConfiguration poolConfig = LettucePoolingClientConfiguration.builder()
+                .clientOptions(clientOptions)
+                .commandTimeout(Duration.ofMillis(redisProperties.getConnectTimeout()))
+                .poolConfig(genericObjectPoolConfig)
+                .build();
+
+        return new LettuceConnectionFactory(config, poolConfig);
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<String, String> ultraFastRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        StringRedisSerializer serializer = new StringRedisSerializer();
+
+        template.setKeySerializer(serializer);
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(serializer);
+        template.setHashValueSerializer(serializer);
+
+        template.setEnableTransactionSupport(false);
+        template.setExposeConnection(false);
+
+        return template;
+    }
+}
