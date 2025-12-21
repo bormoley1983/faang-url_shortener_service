8468b5ad21f9b32ae1ac79275c7e882cab4b05b3
Bimcore <vzeten@gmail.com>
2025-12-21 18:41:05 +0100

    Поправил замечания

--- src/main/java/faang/school/urlshortenerservice/generator/Base62Encoder.java
+++ src/main/java/faang/school/urlshortenerservice/generator/Base62Encoder.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Base62 encoder for generating short URL hashes.
+ * <p>
+ * WARNING: MIN_HASH_LENGTH and BASE_62_CHAR are part of the algorithm.
+ * Changing these values will break compatibility with existing URLs in the database.
+ * Any modification requires full data migration.
+ */
 @Component
 public class Base62Encoder {
     private static final String BASE_62_CHAR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
